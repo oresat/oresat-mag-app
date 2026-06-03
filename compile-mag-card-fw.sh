ab4fa62c410dccf83a1b57a782c741df463b42b6
Theodore Havelka <tedhavelka66@gmail.com>
2026-06-03 15:11:45 +0000

    utils: update shell convenience script for building the mag card Zephyr app.

--- compile-mag-card-fw.sh
+++ compile-mag-card-fw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-west build -b mcxn947_breakoutcard/mcxn947/cpu0 -p -- -DBOARD_ROOT=../../common
+# west build -b mcxn947_breakoutcard/mcxn947/cpu0 -p -- -DBOARD_ROOT=../../common
+west build -b mcxn947_mag_card/mcxn947/cpu0 -p -- -DBOARD_ROOT=.
 exit $?
