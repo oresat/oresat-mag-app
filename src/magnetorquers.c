f3246b5ddb0a22e474bc6de85ba4616f15f55342
Pete Skeggs <plskeggs@noeticdesign.com>
2026-07-15 23:13:27 -0700

    Improve mtmode slightly
    Also fix a bug that prevented setting mode by name from working.

--- src/magnetorquers.c
+++ src/magnetorquers.c
@@ -1219,6 +1219,7 @@ static int cmd_mtmode(const struct shell *sh, size_t argc, char **argv)
 		name = tm_info[i].name;
 		if (strncmp(sel_mode, name, strlen(name)) == 0) {
 			mt_test_mode = (test_modes)i;
+			break;
 		}
 	}
 	if (i >= TM_MODE_COUNT) {
@@ -1249,7 +1250,7 @@ static int cmd_mtmode(const struct shell *sh, size_t argc, char **argv)
 SHELL_CMD_ARG_REGISTER(mtpwm, NULL,  SHELL_HELP("Set/get magnetorquer pwm", "mtpwm  [<axis>] [<new duty cycle value>]"), cmd_mtpwm, 1, 2);
 SHELL_CMD_ARG_REGISTER(frqpwm, NULL, SHELL_HELP("Set/get pwm frequency", "frqpwm [<axis>] [<new frequency in Hz>]"), cmd_frqpwm, 1, 2);
 SHELL_CMD_ARG_REGISTER(ua2pwm, NULL, SHELL_HELP("Set magnetorquer current", "ua2pwm <axis> <target current in uA>"), cmd_ua2pwm, 3, 0);
-SHELL_CMD_ARG_REGISTER(mtmode, NULL, SHELL_HELP("Set/get test mode", "mtmode [<num>] [<axis>]"), cmd_mtmode, 1, 2);
+SHELL_CMD_ARG_REGISTER(mtmode, NULL, SHELL_HELP("Set/get test mode", "mtmode [<num>|name] [<axis>]"), cmd_mtmode, 1, 2);
 #endif // CONFIG_MAGNETORQUER_EXPLORE
 
 K_THREAD_DEFINE(magtqr_id, STACK_SIZE, handle_magnetorquer, NULL, NULL, NULL, PRIORITY, 0, 0);
