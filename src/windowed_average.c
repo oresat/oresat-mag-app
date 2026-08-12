b77d292ec30bc623a1c1e51aeee36306a1a47ea7
Pete Skeggs <plskeggs@noeticdesign.com>
2026-08-12 14:09:26 -0700

    Fix off-by-one error
    In update_windowed_average(), was overwriting 1 entry past the end of
    the buffer.

--- src/windowed_average.c
+++ src/windowed_average.c
@@ -23,8 +23,11 @@ int init_windowed_average(wnd_avg_store *store, int32_t *hist_buffer, int32_t hi
 
 void reset_windowed_average(wnd_avg_store *store)
 {
+	size_t zlen = store->hist_buffer_len * sizeof(store->hist_buffer[0]);
+
 	store->depth = 0;
-	memset(store->hist_buffer, 0, store->hist_buffer_len * sizeof(store->hist_buffer[0]));
+	LOG_DBG("Zeroing hist_buffer for %zd bytes; hist_buffer_len: %d, element: %zd", zlen, store->hist_buffer_len, sizeof(store->hist_buffer[0]));
+	memset(store->hist_buffer, 0, zlen);
 }
 
 int32_t update_windowed_average(wnd_avg_store *store, int32_t new_datum)
@@ -32,7 +35,7 @@ int32_t update_windowed_average(wnd_avg_store *store, int32_t new_datum)
 	int32_t i;
 
 	// throw out oldest sample
-	for (i = store->hist_buffer_len - 1; i >= 0; i--) {
+	for (i = store->hist_buffer_len - 2; i >= 0; i--) {
 		store->hist_buffer[i + 1] = store->hist_buffer[i];
 	}
 
